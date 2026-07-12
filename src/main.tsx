5b2497fa597338fae034404ecdbc1c61e7c679f8
Ahmed <medosamsam919@gmail.com>
2026-07-12 20:36:28 +0300

    feat: initialize Clerk authentication provider and global sonner toaster in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { ClerkProvider } from "@clerk/react";
 import { Toaster } from "sonner";
 import "./index.css";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log(PUBLISHABLE_KEY)
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
